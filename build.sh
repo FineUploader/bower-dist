9a15a67965aaee35ba7dbef3db99743576a88b12
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-05-12 21:21:00 +0700

    Update color output
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# usage color "31;5" "string"
+# usage color "31;0" "string"
 # 0 default
 # 5 blink, 1 strong, 4 underlined
 # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
@@ -12,61 +12,61 @@ e() {
 }
 
 if [ -z $1 ]; then
-	e '31;5' 'Please specify Fine-uploader version you want to build.'
-	e '31;5' 'Example: ./build.sh 5.4.1'
-	e '31;5' '    See: https://github.com/FineUploader/fine-uploader/releases'
+	e '31;0' 'Please specify Fine-uploader version you want to build.'
+	e '31;0' 'Example: ./build.sh 5.4.1'
+	e '31;0' '    See: https://github.com/FineUploader/fine-uploader/releases'
 	exit;
 fi
 
 version=$1
 cmt_msg="Bump v${version}"
 
-e '33;5' 'Entering FineUploader/fine-uploader src directory'
+e '33;0' 'Entering FineUploader/fine-uploader src directory'
 cd src
 git checkout -qf master
 
 if [ -f 'npm-debug.log' ]; then
-	e '33;5' "Seems you've got an error in previous build"
+	e '33;0' "Seems you've got an error in previous build"
 	rm npm-debug.log
 	git branch -qD "v${version}"
 fi
 
-e '33;5' "Check out version $version"
+e '33;0' "Check out version $version"
 git pull -q origin && \
 git checkout -qb "v${version}" $version
 
 if [[ $2 == '--reinstall-dep' ]]; then
-	e '33;5' 'Removing existing node_modules dir'
+	e '33;0' 'Removing existing node_modules dir'
 	rm -rf node_modules;
 fi
 
 if [ ! -d 'node_modules' ]; then
-	e '33;5' 'Installing dependencies'
+	e '33;0' 'Installing dependencies'
 	npm install --quiet;
 fi
 
-e '33;5' 'Build FineUploader/fine-uploader'
+e '33;0' 'Build FineUploader/fine-uploader'
 grunt build_stripped
 
-e '33;5' 'Clean up dist folder'
+e '33;0' 'Clean up dist folder'
 rm -rf ../dist/*
 
-e '33;5' 'Copying build files'
+e '33;0' 'Copying build files'
 cp -rf _build/* ../dist/
 cp -rf client/html/templates/ ../dist/templates
 cp  LICENSE ../dist/
 
-e '33;5' 'Back to master branch'
+e '33;0' 'Back to master branch'
 git checkout master && \
 git branch -D "v${version}"
 
-e '33;5' 'Update version number'
+e '33;0' 'Update version number'
 cd ..
 sed -i -e "s/[0-9].[0-9].[0-9]/${version}/" bower.json
 sed -i -e "s/[0-9].[0-9].[0-9]/${version}/" README.md
 sed -i -e "s/[0-9].[0-9].[0-9]/${version}/" build.sh
 
-e '33;5' "${cmt_msg}"
+e '33;0' "${cmt_msg}"
 git add -A && \
 git commit -am "${cmt_msg}" -q
 
@@ -74,4 +74,4 @@ if [[ $2 = '--release-tag' ]] || [[ $3 = '--release-tag' ]]; then
 	git tag -s $version -m "${cmt_msg}"
 fi
 
-e '32;5' 'Done!'
+e '32;0' 'Done!'
