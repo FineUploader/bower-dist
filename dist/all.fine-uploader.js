4a6eaa195dd4246accfea6ecc0ce129d5b3f9d11
Fery Wardiyanto <ferywardiyanto@gmail.com>
2017-12-13 00:23:54 +0700

    Bump v5.15.4

--- dist/all.fine-uploader.js
+++ dist/all.fine-uploader.js
@@ -1,4 +1,4 @@
-// Fine Uploader 5.15.2 - MIT licensed. http://fineuploader.com
+// Fine Uploader 5.15.4 - MIT licensed. http://fineuploader.com
 (function(global) {
     var qq = function(element) {
         "use strict";
@@ -585,7 +585,7 @@
         };
         qq.Error.prototype = new Error();
     })();
-    qq.version = "5.15.2";
+    qq.version = "5.15.4";
     qq.supportedFeatures = function() {
         "use strict";
         var supportsUploading, supportsUploadingBlobs, supportsFileDrop, supportsAjaxFileUploading, supportsFolderDrop, supportsChunking, supportsResume, supportsUploadViaPaste, supportsUploadCors, supportsDeleteFileXdr, supportsDeleteFileCorsXhr, supportsDeleteFileCors, supportsFolderSelection, supportsImagePreviews, supportsUploadProgress;
@@ -5693,9 +5693,6 @@
             return fileDrag;
         }
         function leavingDocumentOut(e) {
-            if (qq.firefox()) {
-                return !e.relatedTarget;
-            }
             if (qq.safari()) {
                 return e.x < 0 || e.y < 0;
             }
--- dist/dnd.js
+++ dist/dnd.js
@@ -1,4 +1,4 @@
-// Fine Uploader 5.15.2 - MIT licensed. http://fineuploader.com
+// Fine Uploader 5.15.4 - MIT licensed. http://fineuploader.com
 (function(global) {
     var qq = function(element) {
         "use strict";
@@ -578,7 +578,7 @@
             global.qq = qq;
         }
     })();
-    qq.version = "5.15.2";
+    qq.version = "5.15.4";
     qq.supportedFeatures = function() {
         "use strict";
         var supportsUploading, supportsUploadingBlobs, supportsFileDrop, supportsAjaxFileUploading, supportsFolderDrop, supportsChunking, supportsResume, supportsUploadViaPaste, supportsUploadCors, supportsDeleteFileXdr, supportsDeleteFileCorsXhr, supportsDeleteFileCors, supportsFolderSelection, supportsImagePreviews, supportsUploadProgress;
@@ -885,9 +885,6 @@
             return fileDrag;
         }
         function leavingDocumentOut(e) {
-            if (qq.firefox()) {
-                return !e.relatedTarget;
-            }
             if (qq.safari()) {
                 return e.x < 0 || e.y < 0;
             }
